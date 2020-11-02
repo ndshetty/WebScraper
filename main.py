721b41ba019237041e9b7accc0565b1dfdf83b51
ndshetty <ndshetty@ryerson.ca>
2020-11-02 09:52:35 -0500

    BeautifulSoup test

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-import re
-import urllib.request
+#import re
+import requests
+#import urllib.request
 import sys
-response = urllib.request.urlopen('http://example.webscraping.com/places/default/view/India-102')
-html = response.read()
-text = html.decode()
-re.findall('<td class="w2p_fw">(.*?)</td>',text)
+# response = urllib.request.urlopen('http://example.webscraping.com/places/default/view/India-102')
+# html = response.read()
+# text = html.decode()
+# re.findall('<td class="w2p_fw">(.*?)</td>',text)
+# sys.stdout = open("test.txt","w")
+# print(text)
+# sys.stdout.close()
+from bs4 import BeautifulSoup
+page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
+page.status_code
+print(page.status_code)
+page.content
+soup = BeautifulSoup(page.content, 'html.parser')
 sys.stdout = open("test.txt","w")
-print(text)
+print(soup.prettify())
 sys.stdout.close()
-
