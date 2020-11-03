7dc4b5b79d28624aaffc9469dde828260381fc08
ndshetty <ndshetty@ryerson.ca>
2020-11-03 17:05:11 -0500

    integrated into mysql table- test data

--- main.py
+++ main.py
@@ -20,18 +20,34 @@
 
 #import requests
 #import sys
-
-import pandas as pd
 #import seaborn as sns
 #import csv
 
+
+import pandas as pd
+import numpy as py
+import pymysql
+import sqlalchemy
+import mysql.connector
+
+
 url = 'https://en.wikipedia.org/wiki/History_of_Python'
-
 dfs = pd.read_html(url)
-
 df = dfs[0]
-
 df2 = df[['Version', 'Release date']]
+#print(df2)
 
-print(df2)
+database_username = 'root'
+database_password = 'Nihass22!!'
+database_ip = '127.0.0.1'
+database_name = 'ONCovid19'
+database_connection = sqlalchemy.create_engine('mysql+mysqlconnector://{0}:{1}@{2}/{3}'. format(database_username, database_password, database_ip, database_name))
+
+df2.to_sql(con=database_connection, name='test', if_exists='replace')
+
+#conn = pymysql.connect(host='127.0.0.1', user='root', passwd='Nihass22!!', db='ONCovid19')
+#cur = conn.curson()
+
+#cur.execute("USE ONCovid19")
+#cur.execute("SELECT ")
 
