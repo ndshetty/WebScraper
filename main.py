86d1231fc35406c3aeccebfdbaba16497ca1c6b4
ndshetty <ndshetty@ryerson.ca>
2020-11-03 10:37:27 -0500

    playing around with pandas

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 #import re
-import requests
 #import urllib.request
-import sys
 # response = urllib.request.urlopen('http://example.webscraping.com/places/default/view/India-102')
 # html = response.read()
 # text = html.decode()
@@ -9,12 +7,25 @@ import sys
 # sys.stdout = open("test.txt","w")
 # print(text)
 # sys.stdout.close()
-from bs4 import BeautifulSoup
-page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
-page.status_code
-print(page.status_code)
-page.content
-soup = BeautifulSoup(page.content, 'html.parser')
-sys.stdout = open("test.txt","w")
-print(soup.prettify())
-sys.stdout.close()
+
+#from bs4 import BeautifulSoup
+#page = requests.get(" https://data.ontario.ca/dataset/f4112442-bdc8-45d2-be3c-12efae72fb27/resource/455fd63b-603d-4608-8216-7d8647f43350/download/conposco")
+#page.status_code
+#print(page.status_code)
+#page.content
+#soup = BeautifulSoup(page.content, 'html.parser')
+#sys.stdout = open("test.txt","w")
+#print(soup.prettify())
+#sys.stdout.close()
+
+#import requests
+#import sys
+
+import pandas as pd
+#import csv
+
+url = 'https://www.basketball-reference.com/leagues/NBA_2019_per_game.html'
+
+df = pd.read_html(url, header = 0)
+df
+
